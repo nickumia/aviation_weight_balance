1bdf98deda3170b8307911028a5b41b5bcaa597e
nickumia <nickumia6@gmail.com>
2022-06-12 12:57:04 -0400

    New: change navigation icons

--- app/src/main/java/com/ekpro/weightbalance/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/ekpro/weightbalance/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.ekpro.weightbalance.databinding.FragmentGalleryBinding
+import com.ekpro.weightbalance.databinding.FragmentAircraftsBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAircraftsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
                 ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAircraftsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
