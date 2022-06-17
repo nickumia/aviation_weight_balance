b7f91c4af5958cf851578ffc2bf1737dbcea9e5f
nickumia <nickumia6@gmail.com>
2022-06-17 11:06:53 -0400

    fix: variable initilizations and divide by zero

--- app/src/main/java/com/ekpro/weightbalance/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ekpro/weightbalance/ui/home/HomeFragment.kt
@@ -68,54 +68,65 @@ class HomeFragment : Fragment() {
         val button_takeoff: Button = binding.takeoff
         val button_landing: Button = binding.landing
 
-        var v_weight_zero = 0.0F
+        var v_weight_zero = 1.0F
         var v_moment_zero = 0.0F
         var v_moment_ramp = 0.0F
-        var v_weight_ramp = 0.0F
+        var v_weight_ramp = 1.0F
         var v_moment_takeoff = 0.0F
-        var v_weight_takeoff = 0.0F
+        var v_weight_takeoff = 1.0F
 
         button_zero.setOnClickListener(object: View.OnClickListener {
             override fun onClick(p0: View?) {
-                val v_moment_empty = weight_empty.text.toString().toFloat() * arm_empty.text.toString().toFloat()
-                val v_moment_front = weight_front.text.toString().toFloat() * arm_front.text.toString().toFloat()
-                val v_moment_rear = weight_rear.text.toString().toFloat() * arm_rear.text.toString().toFloat()
-                v_moment_zero = v_moment_empty + v_moment_front + v_moment_rear
-                v_weight_zero = weight_empty.text.toString().toFloat() + weight_front.text.toString().toFloat() + weight_rear.text.toString().toFloat()
-                val v_arm_zero = v_moment_zero / v_weight_zero
+                var v_moment_empty = 0.0F
+                var v_moment_front = 0.0F
+                var v_moment_rear = 0.0F
+                try {
+                    v_moment_empty = weight_empty.text.toString().toFloat() * arm_empty.text.toString().toFloat()
+                    v_moment_front = weight_front.text.toString().toFloat() * arm_front.text.toString().toFloat()
+                    v_moment_rear = weight_rear.text.toString().toFloat() * arm_rear.text.toString().toFloat()
+                    v_moment_zero = v_moment_empty + v_moment_front + v_moment_rear
+                    v_weight_zero = weight_empty.text.toString().toFloat() + weight_front.text.toString().toFloat() + weight_rear.text.toString().toFloat()
+                } catch (e: Exception) { }
 
+                val v_arm_zero = v_moment_zero / v_weight_zero
                 moment_empty.text = v_moment_empty.toString()
                 moment_front.text = v_moment_front.toString()
                 moment_rear.text = v_moment_rear.toString()
                 moment_zero.text = v_moment_zero.toString()
                 arm_zero.text = v_arm_zero.toString()
                 weight_zero.text = v_weight_zero.toString()
-                arm_fuel2.text = arm_fuel.text.toString()
-                arm_fuel3.text = arm_fuel.text.toString()
             }
         })
         button_ramp.setOnClickListener(object: View.OnClickListener {
             override fun onClick(p0: View?) {
-                val v_moment_fuel = weight_fuel.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                var v_moment_fuel = 0.0F
 
-                v_moment_ramp = v_moment_zero + v_moment_fuel
-                v_weight_ramp = v_weight_zero + weight_fuel.text.toString().toFloat()
+                try {
+                    v_moment_fuel = weight_fuel.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                    v_moment_ramp = v_moment_zero + v_moment_fuel
+                    v_weight_ramp = v_weight_zero + weight_fuel.text.toString().toFloat()
+                } catch (e: Exception) { }
+
                 val v_arm_ramp = v_moment_ramp / v_weight_ramp
-
                 moment_fuel.text = v_moment_fuel.toString()
                 moment_ramp.text = v_moment_ramp.toString()
                 weight_ramp.text = v_weight_ramp.toString()
                 arm_ramp.text = v_arm_ramp.toString()
+                arm_fuel2.text = arm_fuel.text.toString()
+                arm_fuel3.text = arm_fuel.text.toString()
             }
         })
         button_takeoff.setOnClickListener(object: View.OnClickListener {
             override fun onClick(p0: View?) {
-                val v_moment_taxi = weight_taxi.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                var v_moment_taxi = 0.0F
 
-                v_moment_takeoff = v_moment_ramp - v_moment_taxi
-                v_weight_takeoff = v_weight_ramp - weight_taxi.text.toString().toFloat()
+                try {
+                    v_moment_taxi = weight_taxi.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                    v_moment_takeoff = v_moment_ramp - v_moment_taxi
+                    v_weight_takeoff = v_weight_ramp - weight_taxi.text.toString().toFloat()
+                } catch (e: Exception) { }
+
                 val v_arm_takeoff = v_moment_takeoff / v_weight_takeoff
-
                 moment_taxi.text = v_moment_taxi.toString()
                 moment_takeoff.text = v_moment_takeoff.toString()
                 weight_takeoff.text = v_weight_takeoff.toString()
@@ -124,12 +135,17 @@ class HomeFragment : Fragment() {
         })
         button_landing.setOnClickListener(object: View.OnClickListener {
             override fun onClick(p0: View?) {
-                val v_moment_used = weight_used.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                var v_moment_used = 0.0F
+                var v_moment_landing = 0.0F
+                var v_weight_landing = 1.0F
 
-                val v_moment_landing = v_moment_takeoff - v_moment_used
-                val v_weight_landing = v_weight_takeoff - weight_used.text.toString().toFloat()
+                try {
+                    v_moment_used = weight_used.text.toString().toFloat() * arm_fuel.text.toString().toFloat()
+                    v_moment_landing = v_moment_takeoff - v_moment_used
+                    v_weight_landing = v_weight_takeoff - weight_used.text.toString().toFloat()
+                } catch (e: Exception) { }
+
                 val v_arm_landing = v_moment_landing / v_weight_landing
-
                 moment_used.text = v_moment_used.toString()
                 moment_landing.text = v_moment_landing.toString()
                 weight_landing.text = v_weight_landing.toString()
